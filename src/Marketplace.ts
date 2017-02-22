65c0270d742aa13cdb6f6688bfb0458a8955a031
David Turnbull <david.turnbull.1986@live.com>
2017-02-22 20:08:02 +0000

    Added implementation for removing orders

--- src/Marketplace.ts
+++ src/Marketplace.ts
@@ -6,9 +6,7 @@ export class Marketplace {
     private board: IOrderBoard;
 
     constructor() {
-        this.board = {
-            orders: []
-        };
+        this.board = { orders: [] };
     }
 
     public getBoard() {
@@ -17,6 +15,14 @@ export class Marketplace {
 
     public registerOrder(order: IOrder) {
         this.board.orders.push(order);
+
+        return this.board;
+    }
+
+    public removeOrder(order: IOrder) {
+        if (this.board.orders.indexOf(order) > -1) {
+            this.board.orders.splice(this.board.orders.indexOf(order), 1);
+        }
 
         return this.board;
     }

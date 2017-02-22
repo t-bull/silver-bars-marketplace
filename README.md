cb66b502d04e1e645df17eb17bae198bb0c6cb33
David Turnbull <david.turnbull.1986@live.com>
2017-02-22 18:51:06 +0000

    project setup

---
+++ README.md
@@ -0,0 +1,3 @@
+# silver-bars-marketplace
+
+Simple marketplace trading app for coding test.
---
+++ test/unit/order-board.spec.ts
@@ -0,0 +1,9 @@
+'use strict';
+
+describe('Live Order Board', () => {
+    describe('Given I have an order board', () => {
+        it('Then I can view the live state of the order board', () => {
+            
+        });
+    })
+});

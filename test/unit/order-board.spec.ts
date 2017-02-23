ca77b8ac3beb856ae94610362af6e4be00333047
David Turnbull <david.turnbull.1986@live.com>
2017-02-23 14:23:42 +0000

    uncommented my tests. :/

--- test/unit/order-board.spec.ts
+++ test/unit/order-board.spec.ts
@@ -13,44 +13,44 @@ describe('Live Order Board', () => {
     });
 
     describe('Given I have an order board', () => {
-        // it('Then I can view the live state of the order board', () => {
-        //     const orderBoard: IOrderBoard = marketplace.getBoard();
-        //     Chai.should().exist(orderBoard);
-        //     Chai.assert.equal(orderBoard.orders.length, 0);
-        // });
+        it('Then I can view the live state of the order board', () => {
+            const orderBoard: IOrderBoard = marketplace.getBoard();
+            Chai.should().exist(orderBoard);
+            Chai.assert.equal(orderBoard.orders.length, 0);
+        });
 
-        // it('And orders with the same price should be combined into the same order', () => {
-        //     const firstOrder: IOrder = {
-        //         userId: 'testTrader3',
-        //         type: 'BUY',
-        //         quantity: 5,
-        //         price: 200
-        //     };
+        it('And orders with the same price should be combined into the same order', () => {
+            const firstOrder: IOrder = {
+                userId: 'testTrader3',
+                type: 'BUY',
+                quantity: 5,
+                price: 200
+            };
 
-        //     const secondOrder: IOrder = {
-        //         userId: 'testTrader4',
-        //         type: 'SELL',
-        //         quantity: 10,
-        //         price: 200
-        //     };
+            const secondOrder: IOrder = {
+                userId: 'testTrader4',
+                type: 'SELL',
+                quantity: 10,
+                price: 200
+            };
 
-        //     const thirdOrder: IOrder = {
-        //         userId: 'testTrader4',
-        //         type: 'BUY',
-        //         quantity: 10,
-        //         price: 200
-        //     };
+            const thirdOrder: IOrder = {
+                userId: 'testTrader4',
+                type: 'BUY',
+                quantity: 10,
+                price: 200
+            };
 
-        //     marketplace.registerOrder(firstOrder);
-        //     marketplace.registerOrder(secondOrder);
-        //     marketplace.registerOrder(thirdOrder);
+            marketplace.registerOrder(firstOrder);
+            marketplace.registerOrder(secondOrder);
+            marketplace.registerOrder(thirdOrder);
 
-        //     const board = marketplace.getBoard();
+            const board = marketplace.getBoard();
 
-        //     Chai.should().exist(board);
-        //     Chai.assert.equal(board.orders.length, 2);
-        //     Chai.assert.equal(board.orders[0].quantity, 15);
-        // });
+            Chai.should().exist(board);
+            Chai.assert.equal(board.orders.length, 2);
+            Chai.assert.equal(board.orders[0].quantity, 15);
+        });
 
         it('And all SELL orders should be ordered lowest price first', () => {
              const firstOrder: IOrder = {
@@ -87,40 +87,40 @@ describe('Live Order Board', () => {
          });
     });
 
-    // describe('Given I am a trader', () => {
-    //      it('Then I should be able to register an order', () => {
-    //          const order: IOrder = {
-    //              userId: 'testTrader1',
-    //              type: 'BUY',
-    //              quantity: 3.5,
-    //              price: 303
-    //          };
+    describe('Given I am a trader', () => {
+         it('Then I should be able to register an order', () => {
+             const order: IOrder = {
+                 userId: 'testTrader1',
+                 type: 'BUY',
+                 quantity: 3.5,
+                 price: 303
+             };
 
-    //          const orderBoard = marketplace.registerOrder(order);
+             const orderBoard = marketplace.registerOrder(order);
 
-    //          Chai.should().exist(orderBoard);
-    //          Chai.assert.equal(orderBoard.orders.length, 1);
-    //          Chai.assert.equal(orderBoard.orders[0].userId, order.userId);
-    //          Chai.assert.equal(orderBoard.orders[0].type, order.type);
-    //          Chai.assert.equal(orderBoard.orders[0].quantity, order.quantity);
-    //          Chai.assert.equal(orderBoard.orders[0].price, order.price);
-    //      });
+             Chai.should().exist(orderBoard);
+             Chai.assert.equal(orderBoard.orders.length, 1);
+             Chai.assert.equal(orderBoard.orders[0].userId, order.userId);
+             Chai.assert.equal(orderBoard.orders[0].type, order.type);
+             Chai.assert.equal(orderBoard.orders[0].quantity, order.quantity);
+             Chai.assert.equal(orderBoard.orders[0].price, order.price);
+         });
 
-    //      it('And I should be able to remove my order from the board', () => {
-    //          const order: IOrder = {
-    //              userId: 'testTrader2',
-    //              type: 'BUY',
-    //              quantity: 4,
-    //              price: 300
-    //          };
+         it('And I should be able to remove my order from the board', () => {
+             const order: IOrder = {
+                 userId: 'testTrader2',
+                 type: 'BUY',
+                 quantity: 4,
+                 price: 300
+             };
 
-    //          let orderBoard = marketplace.registerOrder(order);
+             let orderBoard = marketplace.registerOrder(order);
             
-    //          Chai.assert.equal(orderBoard.orders.length, 1);
+             Chai.assert.equal(orderBoard.orders.length, 1);
 
-    //          orderBoard = marketplace.removeOrder(order);
+             orderBoard = marketplace.removeOrder(order);
 
-    //          Chai.assert.equal(orderBoard.orders.length, 0);
-    //      });
-    //  });
+             Chai.assert.equal(orderBoard.orders.length, 0);
+         });
+     });
 });
